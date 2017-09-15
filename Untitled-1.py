3c6f61a4310060fd2e264de555bc0d9599eb5ee6
teriaevip <teriaevip@gmail.com>
2017-09-15 16:33:55 +0400

    qwerty

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+lottery = [3, 42, 12, 19, 30, 59]
+len (lottery)
+lottery.pop(3)
+print(lottery)
---
+++ myvenv1/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\teriaevip\documents\programming\python\myvenv1\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
